8146ef59daf3f87ab5c42cc249a8e21a64a94695
jeetnegi26 <jeetn2612@gmail.com>
2024-09-28 14:41:58 +0530

    Auto Part Initial

---
+++ sumarauto.Model/AutoPartMake.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumarauto.Model
+{
+    public class AutoPartMake : Base
+    {
+        public int Id { get; set; }
+        public int Make_Id { get; set; }
+        public string Model_Title { get; set; }
+        public string Year_Title { get; set; }
+        public string Engine_Title { get; set; }
+        public string Chassis_Title { get; set; }
+        public string Liter_Title { get; set; }
+        public AutoPart AutoPart { get; set; }
+    }
+}
